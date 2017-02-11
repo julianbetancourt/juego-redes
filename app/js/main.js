276b891a7dc6108e97d228d46cecce8894bc3efb
Julian Betancourt <julian@quiqup.com>
2017-02-11 12:02:26 -0500

    bundled js

--- app/js/main.js
+++ app/js/main.js
@@ -44,7 +44,8 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+	__webpack_require__(1);
+	module.exports = __webpack_require__(175);
 
 
 /***/ },
@@ -20400,6 +20401,10 @@
 
 	var _quizResultsJsx2 = _interopRequireDefault(_quizResultsJsx);
 
+	var _data = __webpack_require__(174);
+
+	var _data2 = _interopRequireDefault(_data);
+
 	var App = (function (_React$Component) {
 	  _inherits(App, _React$Component);
 
@@ -20408,57 +20413,7 @@
 
 	    _get(Object.getPrototypeOf(App.prototype), 'constructor', this).call(this, props);
 	    this.state = {
-	      questions: [{
-	        id: 1,
-	        text: 'What does JSON stand for',
-	        choices: [{
-	          id: 'a',
-	          text: 'JavaScript Oriented Notation'
-	        }, {
-	          id: 'b',
-	          text: 'JavaScript Object Notation'
-	        }, {
-	          id: 'c',
-	          text: 'JavaScript Organic Notation'
-	        }],
-	        correct: 'b'
-	      }, {
-	        id: 2,
-	        text: 'Which company mantains ReactJS',
-	        choices: [{
-	          id: 'a',
-	          text: 'Google'
-	        }, {
-	          id: 'b',
-	          text: 'Facebook'
-	        }, {
-	          id: 'c',
-	          text: 'Airbnb'
-	        }],
-	        correct: 'b'
-	      }, {
-	        id: 3,
-	        text: 'Is it an antipattern to include props in the getInitialState method of a component?',
-	        choices: [{
-	          id: 'a',
-	          text: 'Yes'
-	        }, {
-	          id: 'b',
-	          text: 'No'
-	        }],
-	        correct: 'a'
-	      }, {
-	        id: 4,
-	        text: 'Is ReactJS a framework by itself?',
-	        choices: [{
-	          id: 'a',
-	          text: 'Yes'
-	        }, {
-	          id: 'b',
-	          text: 'No'
-	        }],
-	        correct: 'b'
-	      }],
+	      questions: _data2['default'],
 	      score: 0,
 	      current: 1
 	    };
@@ -20539,9 +20494,9 @@
 	      return _react2["default"].createElement(
 	        "div",
 	        { className: "well" },
-	        "Question ",
+	        "Pregunta ",
 	        this.props.current,
-	        " out of ",
+	        " de ",
 	        this.props.questions.length
 	      );
 	    }
@@ -20741,21 +20696,22 @@
 	    value: function render() {
 	      var percent = this.props.score / this.props.questions.length * 100;
 	      if (percent > 80) {
-	        var message = 'You did awesome!';
+	        var message = 'El profe Jhon debe estar orgulloso de tí!';
 	      } else if (percent < 80 && percent > 60) {
-	        var message = 'You did just fine';
+	        var message = 'Aún te hace falta más estudio!';
 	      } else {
-	        var message = 'You did horrible, sorry';
+	        var message = 'Sabes que puedes hacerlo mejor, pónte a estudiar!';
 	      }
+	      var url = window.location.href;
 	      return _react2['default'].createElement(
 	        'div',
 	        { className: 'well' },
 	        _react2['default'].createElement(
 	          'h4',
 	          null,
-	          'You got ',
+	          'Obtuviste ',
 	          this.props.score,
-	          ' out of ',
+	          ' de ',
 	          this.props.questions.length
 	        ),
 	        _react2['default'].createElement(
@@ -20767,8 +20723,8 @@
 	        ),
 	        _react2['default'].createElement(
 	          'a',
-	          { className: 'btn btn-primary', href: 'https://julianbetancourt.github.io/react-simple-quiz/app/' },
-	          'Take test again'
+	          { className: 'btn btn-primary', href: url },
+	          'Reiniciar'
 	        )
 	      );
 	    }
@@ -20779,6 +20735,180 @@
 
 	exports['default'] = Results;
 	module.exports = exports['default'];
+
+/***/ },
+/* 174 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+	exports['default'] = [{
+	  id: 1,
+	  text: 'Qué es el modelo OSI ?',
+	  choices: [{
+	    id: 'a',
+	    text: 'Conjunto de equipos informáticos y software conectados entre sí por medio de dispositivos físicos que envían y reciben impulsos eléctricos'
+	  }, {
+	    id: 'b',
+	    text: 'Una prescripción de caracterizar y estandarizar las funciones de un sistema de comunicaciones en términos de abstracción de capas'
+	  }, {
+	    id: 'c',
+	    text: 'software que permite la interconexión de ordenadores para poder acceder a los servicios y recursos, hardware y software, creando redes de computadoras'
+	  }],
+	  correct: 'b'
+	}, {
+	  id: 2,
+	  text: 'En el modelo OSI, la capa física...',
+	  choices: [{
+	    id: 'a',
+	    text: 'Define las especificaciones eléctricas y físicas de los dispositivos'
+	  }, {
+	    id: 'b',
+	    text: 'Provee independencia de representación de datos (ej. cifrado) mediente la traducción ente los formatos de aplicación y red'
+	  }, {
+	    id: 'c',
+	    text: 'Establece, administra y termina las conexiones entre las aplicaciones locales y remotas'
+	  }],
+	  correct: 'a'
+	}, {
+	  id: 3,
+	  text: 'En el modelo OSI, la capa de enlace de datos...',
+	  choices: [{
+	    id: 'a',
+	    text: 'provee los medios funcionales y de procedimiento para transferir información entre entidades de red '
+	  }, {
+	    id: 'b',
+	    text: 'Provee independencia de representación de datos (ej. cifrado) mediente la traducción ente los formatos de aplicación y red'
+	  }, {
+	    id: 'c',
+	    text: 'Establece, administra y termina las conexiones entre las aplicaciones locales y remotas'
+	  }],
+	  correct: 'a'
+	}, {
+	  id: 4,
+	  text: 'En el modelo OSI, la capa de transporte',
+	  choices: [{
+	    id: 'a',
+	    text: 'provee los medios funcionales y de procedimiento para transferir información entre entidades de red '
+	  }, {
+	    id: 'b',
+	    text: 'establece contexto entre entidades de la capa de aplicación, en los cuales las entidades de capas de más arriba pueden usar sintáxis diferentes y semánticas si el servicio de presentación provee un mapeo entre ellas'
+	  }, {
+	    id: 'c',
+	    text: 'provee una transferencia de datos transparente para el usuario final'
+	  }],
+	  correct: 'c'
+	}, {
+	  id: 5,
+	  text: 'En el modelo OSI, la capa de iterconexión...',
+	  choices: [{
+	    id: 'a',
+	    text: 'provee una transferencia de datos transparente para el usuario final'
+	  }, {
+	    id: 'b',
+	    text: 'No existe'
+	  }, {
+	    id: 'c',
+	    text: 'Establece, administra y termina las conexiones entre las aplicaciones locales y remotas'
+	  }],
+	  correct: 'b'
+	}, {
+	  id: 6,
+	  text: 'Qué es el modelo TCP/IP?',
+	  choices: [{
+	    id: 'a',
+	    text: 'un sistema de comunicación inalámbrico para minimizar las conexiones cableadas'
+	  }, {
+	    id: 'b',
+	    text: 'software que permite la interconexión de ordenadores para poder acceder a los servicios y recursos, hardware y software, creando redes de computadoras'
+	  }, {
+	    id: 'c',
+	    text: 'serie de normas que detallan como deben comunicarse los ordenadores y el modo de interconectar las redes para permitir que diferentes sistemas puedan cooperar compartiendo sus recursos'
+	  }],
+	  correct: 'c'
+	}, {
+	  id: 7,
+	  text: 'Algunas capas del modelo TCP/IP son:',
+	  choices: [{
+	    id: 'a',
+	    text: 'Físico, Internet, Almacenamiento'
+	  }, {
+	    id: 'b',
+	    text: 'Físico, Internet, Transporte'
+	  }, {
+	    id: 'c',
+	    text: 'Físico, Sesión, Presentación'
+	  }],
+	  correct: 'b'
+	}, {
+	  id: 8,
+	  text: 'En el modelo TCP/IP, la capa de Aplicación',
+	  choices: [{
+	    id: 'a',
+	    text: 'No existe'
+	  }, {
+	    id: 'b',
+	    text: 'Establece, administra y termina las conexiones entre las aplicaciones locales y remotas'
+	  }, {
+	    id: 'c',
+	    text: 'es la capa en que los programas más comunes utilizan para comunicarse a través de una red con otros programas'
+	  }],
+	  correct: 'c'
+	}, {
+	  id: 9,
+	  text: 'En la capa de aplicación del modelo TCP/IP, se relacionan los protocolos...',
+	  choices: [{
+	    id: 'a',
+	    text: 'HTTP, FTP, DNS'
+	  }, {
+	    id: 'b',
+	    text: 'Ethernet, Token, TCP, RTP'
+	  }, {
+	    id: 'c',
+	    text: 'Todas las anteriores'
+	  }],
+	  correct: 'a'
+	}, {
+	  id: 10,
+	  text: 'Parte de la popularidad del modelo TCP/IP se debe a...',
+	  choices: [{
+	    id: 'a',
+	    text: 'Independencia del fabricante '
+	  }, {
+	    id: 'b',
+	    text: 'Soporta múltiples tecnologías'
+	  }, {
+	    id: 'c',
+	    text: 'Todas las anteriores'
+	  }],
+	  correct: 'c'
+	}];
+	module.exports = exports['default'];
+
+/***/ },
+/* 175 */
+/***/ function(module, exports) {
+
+	/* WEBPACK VAR INJECTION */(function(__dirname) {'use strict';
+
+	module.exports = {
+	  entry: ['./src/index.js'],
+	  output: {
+	    path: __dirname,
+	    filename: 'app/js/main.js'
+	  },
+	  module: {
+	    loaders: [{
+	      test: /\.jsx?$/,
+	      loader: 'babel',
+	      exclude: /node_modules/
+	    }]
+	  }
+	};
+	/* WEBPACK VAR INJECTION */}.call(exports, "/"))
 
 /***/ }
 /******/ ]);
